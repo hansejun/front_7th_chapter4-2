d3e25c84babadfeab38a535a92a5fe187af48c11
chacha <86880916+hansejun@users.noreply.github.com>
2025-12-23 20:36:59 +0900

    fix: useAutoCallback 타입 문제 수정

--- src/useAutoCallback.ts
+++ src/useAutoCallback.ts
@@ -1,10 +1,12 @@
 import { useCallback, useRef } from 'react';
 
-export function useAutoCallback<T extends (...args: unknown[]) => unknown>(
-  callback: T,
-): T {
+export function useAutoCallback<Args extends unknown[], Return>(
+  callback: (...args: Args) => Return
+): (...args: Args) => Return {
   const callbackRef = useRef(callback);
-  callbackRef.current = callback; // 렌더링마다 업데이트
+  callbackRef.current = callback;
 
-  return useCallback(((...args) => callbackRef.current(...args)) as T, []);
+  return useCallback((...args: Args) => callbackRef.current(...args), []) as (
+    ...args: Args
+  ) => Return;
 }
